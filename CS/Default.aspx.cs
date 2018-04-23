62c1de2a055317461e3d8eb51d3acfa44efdece1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        grid.DataSource = Enumerable.Range(0, 10).Select(x => new SomeData { ProductID = x, StartDate = DateTime.Now, EndDate = DateTime.Now });
+        grid.DataBind();
+    }
+    protected void grid_RowUpdating(object sender, DevExpress.Web.Data.	ASPxDataUpdatingEventArgs e) {
+		e.Cancel = true;
+	}
+	protected void grid_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e) {
+		e.Cancel = true;
+	}
+	protected void grid_RowDeleting(object sender, DevExpress.Web.Data.ASPxDataDeletingEventArgs e) {
+		e.Cancel = true;
+	}
+	
+}
+public class SomeData
+{
+    public int ProductID { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to implement a date range picker in ASPxGridView using a column's field name
+
+
+This example demonstrates how to implement a date range picker in ASPxGridView using a column's field name.
+
+
+<h3>Description</h3>
+
+&nbsp;
+
+<br/>
+
+
